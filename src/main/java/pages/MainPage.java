d49f0e4762d5630e15d65207433ed433b6cac088
Evgeny Usatenko <usatenko_e@mail.ru>
2022-10-29 00:04:06 +0300

    Подключил (настроил) запуск в Firefox. Исправил ошибки с ожиданием элемента в "Вопросы о важном"

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
 
@@ -18,25 +20,28 @@ public class MainPage {
 
     // Кликнуть по строке в "Вопросы о важном"
     public void clickAccordeonHeading(String locatorAccordeonHeading) {
-        //Явное ожидание на появление текста (не больше 3 секунд)
-        WebElement element = driver.findElement(By.id(String.valueOf(locatorAccordeonHeading)));
+        WebElement element = driver.findElement(By.id(locatorAccordeonHeading));
+        // Скролл до элемента
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        // Явное ожидание видимости элемента
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.elementToBeClickable(By.id(locatorAccordeonHeading)));
         // Кликаем по строке
         element.click();
     }
 
     // Получить текст из строки "Вопросы о важном"
     public String getTextAccordeonHeading(String locatorAccordeonHeading) {
-        //Явное ожидание на появление текста (не больше 3 секунд)
         WebElement element = driver.findElement(By.id(String.valueOf(locatorAccordeonHeading)));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-
         String actualTextAccordeonHeader = element.getText();
         return actualTextAccordeonHeader;
     }
 
     // Получить текст из выпавшей панели (после клика по строке в "Вопросы о важном)
     public String getTextAccordeonPanel (String locatorAccordeonPanel) {
+        // Явное ожидание видимости элемента
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id(locatorAccordeonPanel)));
         String actualTextAccordeonPanel = driver.findElement(By.id(locatorAccordeonPanel)).getText();
         return actualTextAccordeonPanel;
     }
